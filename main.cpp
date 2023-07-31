0fa136987569196ccbb6979588cf4945d0a01c5b
Joseph Gale <jgale2@wgu.edu>
2023-07-30 22:26:09 -0700

    remove line items

--- main.cpp
+++ main.cpp
@@ -1,10 +1,3 @@
-/*
-Scripting and Programming - Applications - C867
-Joseph Gale
-jgale2@wgu.edu
-Student ID: 000457912
-*/
-
 #include <iostream>
 #include "roster.h"
 
@@ -21,7 +14,7 @@ int main()
         "A2,Suzan,Erickson,Erickson_1990@gmailcom,19,50,30,40,NETWORK",
         "A3,Jack,Napoli,The_lawyer99yahoo.com,19,20,40,33,SOFTWARE",
         "A4,Erin,Black,Erin.black@comcast.net,22,50,58,40,SECURITY",
-        "A5,Joseph,Gale,jgale2@wgu.edu,31,10,25,29,SOFTWARE"};
+        "A5,John,Glam,xxx@xxx.com,31,10,25,29,SOFTWARE"};
 
     for (int i = 0; i < 5; i++)
     {
@@ -30,30 +23,30 @@ int main()
 
     cout << "Course Title: Scripting and Programming - Applications - C867" << endl;
     cout << "Programming Language: C++" << endl;
-    cout << "WGU Student ID: 000457912" << endl;
-    cout << "Your Name: Joseph Gale" << endl;
+    cout << "WGU Student ID: xxx" << endl;
+    cout << "Your Name: xxxx" << endl;
     cout << endl;
     cout << " " << endl;
-  
+
     cout << "Complete Roster: " << endl;
     classRoster.printAll();
     cout << " " << endl;
     cout << " " << endl;
-  
+
     classRoster.printInvalidEmails();
     cout << " " << endl;
-    //printing the average days in the courses for each student
+    // printing the average days in the courses for each student
     cout << "Average days in course: " << endl;
     for (int i = 0; i <= classRoster.getLastIndex(); ++i)
     {
-        Student* student = classRoster.getStudentAt(i);
+        Student *student = classRoster.getStudentAt(i);
         if (student != nullptr)
         {
             string studentID = student->getStudentID();
             classRoster.printAverageDaysInCourse(studentID);
         }
     }
-    
+
     cout << " " << endl;
     cout << " " << endl;
 
@@ -61,7 +54,7 @@ int main()
     classRoster.printByDegreeProgram(SOFTWARE);
     cout << " " << endl;
     cout << " " << endl;
-    
+
     classRoster.remove("A3");
     cout << " " << endl;
     cout << " " << endl;
@@ -72,6 +65,6 @@ int main()
     cout << " " << endl;
 
     classRoster.remove("A3");
-    
+
     return 0;
 }
